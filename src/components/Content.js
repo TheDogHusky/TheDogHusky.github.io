dd017bf5707c4f7079a4397f2230c840bac83b6d
TheDogHusky <leredlabo@gmail.com>
2023-02-04 22:58:29 +0100

    Fixed some grammatical problems

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,7 +6,7 @@ class Content extends React.Component {
     return (
       <div id="scroll" className="Content">
         <h2>About Me</h2>
-        <p className="AboutMe">Hey! My name's Adam Billard. I am mostly known as ClassyCrafter. I'm a young musician and developer inspiring to be a professional artist. I started learning guitar at 13 years old, some years after learning solfege. I learnt piano too (at the same time). I started making musics several months later with my first music "I don't wanna suffer again". Then, I made a band with some of my friends and we are making an end-of-year concert at my middle school.</p>
+        <p className="AboutMe">Hey! My name's Adam Billard. I am mostly known as ClassyCrafter. I'm a young musician and developer inspiring to be a professional artist. I started learning guitar at 13 years old, some years after learning solfege. I learnt piano too (at the same time). I started making music several months later with my first music "I don't wanna suffer again". Then, I made a band with some of my friends and we are making an end-of-year concert at my middle school.</p>
       </div>
     );
   };
