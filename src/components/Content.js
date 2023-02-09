acd0e60089f6f82b753d129d9dd700942682f412
TheDogHusky <leredlabo@gmail.com>
2023-02-09 17:19:00 +0100

    Cleaned up classes, added some responsive content for the title

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,8 +5,8 @@ class Content extends React.Component {
   render() {
     return (
       <div id="scroll" className="Content">
-        <h2 className="fade-in-enabled">About Me</h2>
-        <p className="AboutMe fade-in-enabled">
+        <h2 className="fie">About Me</h2>
+        <p className="About-me fie">
             Hey! My name's Adam Billard. I am mostly known as ClassyCrafter. I'm a young musician and developer aspiring to be a professional artist. I started learning guitar and piano at 13 years old some years after learning solfege. I started making music several months later with my first song "I Don't Wanna Suffer Again." Then, I started a band with some of my friends, and we are making an end-of-year concert at my middle school.
         </p>
       </div>
