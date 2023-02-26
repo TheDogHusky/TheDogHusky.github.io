62e1e03de06ff87caa4d69fa8044c7497bdc3a5a
TheDogHusky <leredlabo@gmail.com>
2023-02-26 16:25:43 +0100

    fixed some problems

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,8 +6,8 @@ class Content extends React.Component {
     return (
       <div id="scroll" className="Content">
         <h2 className="fie">My New Song</h2>
-        <p className="Song-title fie">"I Don't Wanna Suffer Again", being released on February 26!</p>
-        <a href="https://www.youtube.com/watch?v=Hal46x0INes"><img className="Song-image fie" src="/idwsa.webp" alt="Song Image" /></a>
+        <p className="Song-title fie">"I Don't Wanna Suffer Again", realeased on <a className="Song-link" href="" target="_blank">SoundCloud</a> and <a className="Song-link" href="" target="_blank">YouTube</a>!</p>
+        <a href="https://www.youtube.com/watch?v=Hal46x0INes" target="_blank" className="Song-image-a"><img className="Song-image fie" src="/idwsa.webp" alt="Song Image" /></a>
         <h2 className="fie">About Me</h2>
         <p className="About-me fie">
             Hey! My name's Adam Billard. I am mostly known as ClassyCrafter. I'm a young musician and developer aspiring to be a professional artist. I started learning guitar and piano at 13 years old some years after learning solfege. I started making music several months later with my first song "I Don't Wanna Suffer Again." Then, I started a band with some of my friends, and we are making an end-of-year concert at my middle school.
