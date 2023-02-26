8f652cf5afa986a230fc05cf52ae48f3f893af00
TheDogHusky <leredlabo@gmail.com>
2023-02-26 16:16:43 +0100

    forgot the fie classes

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,9 +5,9 @@ class Content extends React.Component {
   render() {
     return (
       <div id="scroll" className="Content">
-        <h2>My New Song</h2>
-        <p className="Song-title">"I Don't Wanna Suffer Again", being released on February 26!</p>
-        <a href="https://www.youtube.com/watch?v=Hal46x0INes"><img className="Song-image" src="/idwsa.webp" alt="Song Image" /></a>
+        <h2 className="fie">My New Song</h2>
+        <p className="Song-title fie">"I Don't Wanna Suffer Again", being released on February 26!</p>
+        <a href="https://www.youtube.com/watch?v=Hal46x0INes"><img className="Song-image fie" src="/idwsa.webp" alt="Song Image" /></a>
         <h2 className="fie">About Me</h2>
         <p className="About-me fie">
             Hey! My name's Adam Billard. I am mostly known as ClassyCrafter. I'm a young musician and developer aspiring to be a professional artist. I started learning guitar and piano at 13 years old some years after learning solfege. I started making music several months later with my first song "I Don't Wanna Suffer Again." Then, I started a band with some of my friends, and we are making an end-of-year concert at my middle school.
